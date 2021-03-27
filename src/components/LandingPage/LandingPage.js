4727b44a76acc2bdebd581a9b13adfc6348ddc9f
Pol Piella <info@polpiellamusic.com>
2021-03-27 16:05:22 +0000

    Remove styled-components to fix netlify build

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import img from "../../assets/profile.png";
-import ProfilePicture from './ProfilePicture';
-
-const LandingPageContainer = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-items: center;
-`
+import "./landing-page.css";
 
 const LandingPage = () => (
-    <LandingPageContainer>
-        <ProfilePicture src={img} />
+    <div className="landing-page">
+        <img className="profile-image" src={img} />
         <h1>Hi! I'm Pol</h1>
         <h1>I am a software engineer focused on building outstanding mobile and web applications. While I finish building my site, feel free to check out my work on Github or drop me a message</h1>
-    </LandingPageContainer>
+    </div>
 );
 
 export default LandingPage;
