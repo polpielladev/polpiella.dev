1539e18c1adcfff7800de35e61995406931d5c3a
Pol Piella <info@polpiellamusic.com>
2021-03-31 21:18:48 +0100

    Update font type and add typography styles

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from "react-dom";
 import React from "react";
 import LandingPage from "./components/LandingPage/LandingPage";
 import "./global-styles.scss"
+import "./typography.scss"
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
