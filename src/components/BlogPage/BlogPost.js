22882eea732014ae2f9f96d28a283915d8fbc601
Pol Piella <info@polpiellamusic.com>
2021-04-22 21:01:21 +0100

    Remove print statement

--- src/components/BlogPage/BlogPost.js
+++ src/components/BlogPage/BlogPost.js
@@ -3,35 +3,31 @@ import { Link } from "react-router-dom";
 import "./blog-page.scss";
 import PostTag from "./PostTag";
 
-const BlogPost = ({ post }) => {
-    console.log(post);
-
-    return (
-        <div className="blog-post-container">
-            <Link to={`/${post.slug}`}>
-                <h3>{post.title.toUpperCase()}</h3>
-            </Link>
-            <div className="tags">
-                {post.tags.map((tag) => (
-                    <PostTag title={tag.name} key={tag.name} />
-                ))}
-            </div>
-            <p>{post.excerpt}</p>
-            <div className="metadata">
-                <p>{new Date(post.published_at).toDateString()}</p>
-                <p>-</p>
-                <p>{`📖  ${post.reading_time} minutes`}</p>
-            </div>
-            <div className="author-section">
-                <img src={post.authors[0].profile_image} />
-                <div className="author-metadata">
-                    <p>
-                        WRITTEN BY: <b>{post.authors[0].name}</b>
-                    </p>
-                </div>
+const BlogPost = ({ post }) => (
+    <div className="blog-post-container">
+        <Link to={`/${post.slug}`}>
+            <h3>{post.title.toUpperCase()}</h3>
+        </Link>
+        <div className="tags">
+            {post.tags.map((tag) => (
+                <PostTag title={tag.name} key={tag.name} />
+            ))}
+        </div>
+        <p>{post.excerpt}</p>
+        <div className="metadata">
+            <p>{new Date(post.published_at).toDateString()}</p>
+            <p>-</p>
+            <p>{`📖  ${post.reading_time} minutes`}</p>
+        </div>
+        <div className="author-section">
+            <img src={post.authors[0].profile_image} />
+            <div className="author-metadata">
+                <p>
+                    WRITTEN BY: <b>{post.authors[0].name}</b>
+                </p>
             </div>
         </div>
-    );
-};
+    </div>
+);
 
 export default BlogPost;
