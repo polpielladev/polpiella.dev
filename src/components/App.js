bd0beda2c79d7c39ee8bcc204cf4efa1f4d47fca
Pol Piella <info@polpiellamusic.com>
2021-04-30 11:51:12 +0100

    Use BrowserRouter instead

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HashRouter, Link } from "react-router-dom";
+import { BrowserRouter, HashRouter, Link } from "react-router-dom";
 import BlogPage from "./BlogPage/BlogPage";
 
 const App = () => {
@@ -7,14 +7,14 @@ const App = () => {
 
     return (
         <div className={`app ${darkMode ? "dark-mode" : ""}`}>
-            <HashRouter>
+            <BrowserRouter>
                 <header>
                     <Link to="/">
                         <p className="home-button">HOME</p>
                     </Link>
                 </header>
                 <BlogPage />
-            </HashRouter>
+            </BrowserRouter>
         </div>
     );
 };
