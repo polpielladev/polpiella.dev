336c1914090552a6cc01120b2cff5c20cddb4a2b
Pol Piella <info@polpiellamusic.com>
2021-05-02 18:25:48 +0100

    Add favicon for site

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/global/global-styles.scss";
 import "../styles/global/typography.scss";
 import "../styles/global/code-style.scss";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
     return (
         <div className="app dark-mode">
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Component {...pageProps} />
         </div>
     );
