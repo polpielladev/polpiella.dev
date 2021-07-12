373c409d45a1211a15118e3a32061bf5bbc79364
Pol Piella <info@polpiellamusic.com>
2021-07-12 16:49:44 +0100

    Always get paths using `path.resolve`

--- models/API.js
+++ models/API.js
@@ -1,12 +1,12 @@
-import fs from 'fs'
-import { join } from 'path'
-import matter from 'gray-matter'
-import _ from 'lodash';
-
-const postsDirectory = join(process.cwd(), '_posts')
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import _ from "lodash";
+import path from "path";
 
 export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory).map((file) => file.replace(/\.md$/, ''))
+    const directory = path.resolve("./_posts");
+    return fs.readdirSync(directory).map((file) => file.replace(/\.md$/, ""));
 }
 
 export function getTagForSlug(slug) {
@@ -14,58 +14,70 @@ export function getTagForSlug(slug) {
 }
 
 export function getAllTags() {
-    return _.uniqBy(getAllPosts(['tags']).map((post) => post.tags).flat(), (tag) => tag.slug)
+    return _.uniqBy(
+        getAllPosts(["tags"])
+            .map((post) => post.tags)
+            .flat(),
+        (tag) => tag.slug
+    );
 }
 
 export function getBlogPostsForTag(slug) {
-    return getAllPosts().filter((post) => post.tags.some((tag) => tag.slug === slug));
+    return getAllPosts().filter((post) =>
+        post.tags.some((tag) => tag.slug === slug)
+    );
 }
 
-export function getPostBySlug(slug, fields = [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-    'readtime',
-    'tags',
-    'content'
-  ]) {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+export function getPostBySlug(
+    slug,
+    fields = [
+        "title",
+        "date",
+        "slug",
+        "author",
+        "coverImage",
+        "excerpt",
+        "readtime",
+        "tags",
+        "content",
+    ]
+) {
+    const realSlug = slug.replace(/\.md$/, "");
+    const fullPath = path.resolve("./_posts", `${realSlug}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const { data, content } = matter(fileContents);
 
-  const items = {}
+    const items = {};
 
-  fields.forEach((field) => {
-    if (field === 'slug') {
-      items[field] = realSlug
-    }
-    if (field === 'content') {
-      items[field] = content
-    }
+    fields.forEach((field) => {
+        if (field === "slug") {
+            items[field] = realSlug;
+        }
+        if (field === "content") {
+            items[field] = content;
+        }
 
-    if (data[field]) {
-      items[field] = data[field]
-    }
-  })
+        if (data[field]) {
+            items[field] = data[field];
+        }
+    });
 
-  return items
+    return items;
 }
 
-export function getAllPosts(fields = [
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-    'readtime',
-    'tags'
-  ]) {
-  return getPostSlugs()
-    .map((slug) => getPostBySlug(slug, fields))
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+export function getAllPosts(
+    fields = [
+        "title",
+        "date",
+        "slug",
+        "author",
+        "coverImage",
+        "excerpt",
+        "readtime",
+        "tags",
+    ]
+) {
+    return getPostSlugs()
+        .map((slug) => getPostBySlug(slug, fields))
+        .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
 }
