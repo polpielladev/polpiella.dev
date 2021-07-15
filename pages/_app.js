5ba4dd707963aa4b8ff27a05370548238108dcf8
Pol Piella <info@polpiellamusic.com>
2021-07-15 11:04:20 +0100

    Add twittter image from home page

--- pages/_app.js
+++ pages/_app.js
@@ -27,42 +27,58 @@ export default function App({ Component, pageProps }) {
     }, []);
 
     return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-            <CodeThemeStyle />
-            <GlobalStyles />
-            <div className="container">
-                <Head>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link
-                        rel="alternate"
-                        type="application/rss+xml"
-                        title="RSS feed of my website's latest posts"
-                        href="https://polpiella.codes/rss.xml"
-                    />
-                </Head>
-                <div className="app">
-                    <Header>
-                        <Link href="/">
-                            <a>
-                                <img
-                                    className="home-button"
-                                    src="/icons/home.svg"
-                                    alt="home"
-                                />
-                            </a>
-                        </Link>
-                        <DarkModeToggle
-                            checked={darkMode}
-                            onChange={(value) => {
-                                window.localStorage.setItem("dark-mode", value);
-                                setDarkMode(value);
-                            }}
-                            size={60}
-                        />
-                    </Header>
-                    <Component {...pageProps} />
+        <>
+            <Head>
+                <meta
+                    property="og:image"
+                    content="https://blog-og-image-eight.vercel.app/**Pol%20Piella%20Codes**%20-%20Blog.png?theme=dark&md=1&fontSize=100px"
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@polcodes" />
+                <meta name="twitter:title" content="Pol Piella Codes - Blog" />
+                <meta
+                    name="twitter:description"
+                    content="A blog where I talk about software development topics in languages like Swift, Javascript and using frameworks such as Next.js, React, Combine and many more!"
+                />
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="alternate"
+                    type="application/rss+xml"
+                    title="RSS feed of my website's latest posts"
+                    href="https://polpiella.codes/rss.xml"
+                />
+            </Head>
+            <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+                <CodeThemeStyle />
+                <GlobalStyles />
+                <div className="container">
+                    <div className="app">
+                        <Header>
+                            <Link href="/">
+                                <a>
+                                    <img
+                                        className="home-button"
+                                        src="/icons/home.svg"
+                                        alt="home"
+                                    />
+                                </a>
+                            </Link>
+                            <DarkModeToggle
+                                checked={darkMode}
+                                onChange={(value) => {
+                                    window.localStorage.setItem(
+                                        "dark-mode",
+                                        value
+                                    );
+                                    setDarkMode(value);
+                                }}
+                                size={60}
+                            />
+                        </Header>
+                        <Component {...pageProps} />
+                    </div>
                 </div>
-            </div>
-        </ThemeProvider>
+            </ThemeProvider>
+        </>
     );
 }
