60f3197dd58791b14de68386e4765834abbd136f
Pol Piella <info@polpiellamusic.com>
2021-08-10 18:55:13 +0200

    Use page screenshot as OG image

---
+++ pages/api/thumbnail.js
@@ -0,0 +1,36 @@
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
+
+const getAbsoluteURL = (path) => {
+    const baseURL = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:8080";
+    return baseURL + path;
+};
+
+export default async (req, res) => {
+    const browser = await playwright.chromium.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+
+    const page = await browser.newPage({
+        viewport: {
+            width: 1200,
+            height: 630,
+        },
+    });
+
+    const url = getAbsoluteURL(req.query["path"] || "");
+    await page.goto(url, {
+        timeout: 15 * 1000,
+    });
+    const data = await page.screenshot({
+        type: "png",
+    });
+    await browser.close();
+    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
+    res.setHeader("Content-Type", "image/png");
+    res.end(data);
+};
