afde87d1b9bb580b87024e747b53231c9274167c
Pol Piella <info@polpiellamusic.com>
2021-08-15 11:33:35 +0200

    Attempt to fix thumbnail route by not importing

--- api/thumbnail.js
+++ api/thumbnail.js
@@ -1,6 +1,12 @@
 import chromium from "chrome-aws-lambda";
 import playwright from "playwright-core";
-import absoluteURL from "utils/absoluteURL";
+
+export default function absoluteURL(path) {
+    const baseURL = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:8080";
+    return baseURL + path;
+}
 
 export default async (req, res) => {
     const browser = await playwright.chromium.launch({
