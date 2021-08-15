9770d71d5239a85fe6775e9140a54d70138f25d6
Pol Piella <info@polpiellamusic.com>
2021-08-15 14:35:06 +0200

    Add custom styles and load them to `playwright`

--- api/thumbnail.js
+++ api/thumbnail.js
@@ -1,13 +1,85 @@
 import playwright from "playwright-core";
 import chromium from "chrome-aws-lambda";
 
+function getCSS() {
+    return `
+    *,
+    :after,
+    :before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    h1 {
+        font-size: 50px;
+        max-width: 800px;
+    }
+
+    p {
+        font-size: 20px;
+        color: darkgrey;
+    }
+
+    body {
+        width: 100vw;
+        height: 100vh;
+        background-color: #282c35;
+        color: white;
+        display: flex;
+        align-items: center;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        padding: 0 20px;
+    }
+
+    .container > *:not(:last-child) {
+        margin-bottom: 30px;
+    }
+
+    img {
+        border-radius: 20%;
+        height: auto;
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+    }
+    `;
+}
+
+function getHTML(title, readTime) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <title>Document</title>
+            <style>${getCSS()}</style>
+        </head>
+        <body>
+            <div class="container">
+                <img src="/assets/profile.png" />
+                <h1>${title}</h1>
+                <p>Read time: ${readTime} minutes</p>
+            </div>
+        </body>
+    </html>
+    `;
+}
+
 export default async (req, res) => {
     try {
         await chromium.font(
             "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
         );
         const { query } = req;
-        const { title } = query;
+        const { title, readTime } = query;
         const browser = await playwright.chromium.launch({
             args: chromium.args,
             executablePath: await chromium.executablePath,
@@ -18,7 +90,7 @@ export default async (req, res) => {
             viewport: { width: 1200, height: 630 },
         });
 
-        await page.setContent(`<h1>${title}</h1>`);
+        await page.setContent(getHTML(title, readTime));
         const data = await page.screenshot({ type: "png" });
         await browser.close();
         res.setHeader(
