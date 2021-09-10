931f5999d43f17c1076c2ce6f788a0848b258248
Pol Piella <info@polpiellamusic.com>
2021-09-10 12:16:07 +0100

    Use  instead of  on  page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,73 @@
-import Head from "next/head";
 import BlogList from "components/BlogList";
-import styles from "styles/pages/BlogPage.module.scss";
 import { getAllPosts } from "models/API";
 import profileImage from "public/assets/profile.png";
 import generateRSSFeed from "services/generateRSSFeed";
 import fs from "fs";
 import Image from "next/image";
 import SEO from "components/SEO";
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin-top: 40px;
+`;
+
+const Intro = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin-bottom: 40px;
+    align-self: center;
+
+    @media screen and (max-width: 810px) {
+        flex-direction: column;
+    }
+
+    p {
+        width: 70%;
+    }
+`;
+
+const ProfileImageContainer = styled.div`
+    width: 30%;
+
+    div {
+        position: unset !important;
+    }
+
+    img {
+        object-fit: contain;
+        width: 100% !important;
+        position: relative !important;
+        height: unset !important;
+        border-radius: 20%;
+    }
+`;
+
+const IntroSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    align-items: flex-start;
+    padding-left: 30px;
+    height: 100%;
+    justify-content: center;
+
+    p {
+        width: 100%;
+    }
+
+    @media screen and (max-width: 810px) {
+        p {
+            width: 85%;
+        }
+        text-align: center;
+        align-items: center;
+        padding-left: 0;
+        margin-top: 10px;
+    }
+`;
 
 export async function getStaticProps() {
     const posts = getAllPosts();
@@ -26,33 +87,29 @@ export default function BlogPage({ posts }) {
                 description="A blog where I talk about software development topics in languages like Swift, Javascript and using frameworks such as Next.js, React, Combine and many more!"
                 caption=""
             />
-            <div className={styles.blogPageContainer}>
-                <div className={styles.postList}>
-                    <div className={styles.blogIntro}>
-                        <div className={styles.profileContainer}>
-                            <Image
-                                className={styles.profileImage}
-                                alt="Author Profile Image"
-                                src={profileImage}
-                                layout="fill"
-                                objectFit="cover"
-                            />
-                        </div>
-
-                        <div className={styles.introSection}>
-                            <h2>
-                                <b>Hi! I&apos;m Pol</b> 🤘
-                            </h2>
-                            <p>
-                                Welcome to my personal blog! I am an iOS
-                                developer based in the UK and I love talking
-                                about software development in general.
-                            </p>
-                        </div>
-                    </div>
-                    <BlogList posts={posts} />
-                </div>
-            </div>
+            <Container>
+                <Intro>
+                    <ProfileImageContainer>
+                        <Image
+                            alt="Author Profile Image"
+                            src={profileImage}
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </ProfileImageContainer>
+                    <IntroSection>
+                        <h2>
+                            <b>Hi! I&apos;m Pol</b> 🤘
+                        </h2>
+                        <p>
+                            Welcome to my personal blog! I am an iOS developer
+                            based in the UK and I love talking about software
+                            development in general.
+                        </p>
+                    </IntroSection>
+                </Intro>
+                <BlogList posts={posts} />
+            </Container>
         </>
     );
 }
