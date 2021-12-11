ce4a34ee3b4f152c1297df52168f02170996f79c
Pol Piella Abadia <hi@polpiella.dev>
2021-12-11 19:11:35 +0000

    Uses prose-invert to support dark mode text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,4 @@ module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   plugins: [require("@tailwindcss/typography")],
-  variants: {
-    typography: ["responsive", "dark"],
-  },
 }
