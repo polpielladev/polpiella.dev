664871dd20656797949e6738d9fa92d42c2155ba
Pol Piella Abadia <hi@polpiella.dev>
2021-12-19 20:27:08 +0000

    Renames purge to content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+  content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   plugins: [require("@tailwindcss/typography")],
 }
