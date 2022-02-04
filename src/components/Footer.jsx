125a23f0bd6cf0a93b4e5715940960c92472ef89
Pol Piella Abadia <hi@polpiella.dev>
2022-02-04 09:49:34 +0000

    Fixes slight issues with React migration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
     return (
         <footer className="flex flex-col mt-16 align-middle bg-gray-100 mb-0 dark:bg-gray-800">
             <div className="flex align-middle justify-center mt-5 mb-7 gap-8" >
-                {SOCIAL_MEDIA.map(social => <a className="hover:scale-110 transition-transform dark:invert" target="_blank" rel="noopener noreferrer" href={social.url}><img width="25px" height="25px" src={social.asset} /></a>)}
+                {SOCIAL_MEDIA.map(social => <a className="hover:scale-110 transition-transform dark:invert" key={social.url} target="_blank" rel="noopener noreferrer" href={social.url}><img width="25px" height="25px" src={social.asset} /></a>)}
             </div>
             <p className=" text-center text-gray-600 mb-1 dark:text-gray-300">Made with 💛 by Pol Piella Abadia</p>
             <p className=" text-center text-gray-600 mb-5 dark:text-gray-300">Code block font: <a className="underline font-bold hover:text-amber-500 decoration-amber-400 decoration-wavy" href="https://www.monolisa.dev/buy" target="_blank" rel="noopener noreferrer">MonoLisa</a></p>
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,6 +1,6 @@
 export default function Tags({ tags }) {
     return (
     <div className="flex flex-wrap gap-2 mb-0">
-        {tags.map(tag => <a href={`/category/${tag.slug}`} className="bg-amber-300 p-1 rounded text-xs font-bold text-gray-900 no-underline">{tag.name}</a>)}
+        {tags.map(tag => <a href={`/category/${tag.slug}`} key={tag.slug} className="bg-amber-300 p-1 rounded text-xs font-bold text-gray-900 no-underline">{tag.name}</a>)}
     </div>)
 }
