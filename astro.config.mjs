c9e13c7e90c5ddf13a7f5d2296d2ea0bce38eb41
Pol Piella Abadia <hi@polpiella.dev>
2022-02-04 10:19:19 +0000

    Autolinks headings using `rehype` plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
+import astroRemark from '@astrojs/markdown-remark'
+
 export default {
     buildOptions: {
         site: "https://polpiella.dev",
         sitemap: true,
     },
+    markdownOptions: {
+		render: [
+			astroRemark,
+			{
+				rehypePlugins: [
+                    'rehype-slug',
+					['rehype-autolink-headings', { behavior: 'wrap' }],
+				],
+			},
+		],
+	},
     renderers: ['@astrojs/renderer-react'],
 }
