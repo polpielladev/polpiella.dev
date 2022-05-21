81c50b0184badebc4a8a72f85146bbf94705424f
Pol Piella Abadia <hi@polpiella.dev>
2022-05-21 11:41:15 +0100

    Updates `site` to harcoded string to fix error

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss from '@astrojs/rss'
 
 export const get = () =>
   rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: import.meta.env.SITE,
+    title: "Pol Piella's Blog",
+    description: 'A blog about iOS app development, software development, and other things.',
+    site: 'https://polpiella.dev',
     items: import.meta.glob('./*.md'),
     customData: `<language>en-us</language>`,
   })
