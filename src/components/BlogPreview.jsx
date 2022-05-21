7cccd1f10528f5629c01b907aea0934a7464201a
Pol Piella Abadia <hi@polpiella.dev>
2022-05-21 12:36:09 +0100

    Fixes invalid date error after frontmatter rename

--- src/components/BlogPreview.jsx
+++ src/components/BlogPreview.jsx
@@ -2,20 +2,22 @@ import Metadata from './Metadata'
 import Tags from './Tags'
 
 export default function BlogPreview({ post }) {
-  const { slug, title, excerpt, date, readtime, tags } = post.frontmatter
-  const formattedDate = new Date(date).toDateString()
+  const { slug, title, excerpt, pubDate, readtime, tags } = post.frontmatter
+  const formattedDate = new Date(pubDate).toDateString()
 
   return (
-    <div className="prose flex flex-col gap-1 dark:prose-invert">
-      <h3 className=" m-0 font-bold text-zinc-400">{formattedDate}</h3>
-      <a href={`/${slug}`} className="m-0 rounded no-underline">
-        <h1 className="m-0 hover:text-amber-900 dark:hover:text-amber-300">
-          {title}
-        </h1>
-      </a>
-      <p className="m-0">{excerpt}</p>
-      <Metadata className="m-0" readtime={readtime} />
-      <Tags className="m-0" tags={tags} />
+    <div className="prose flex flex-col dark:prose-invert">
+      <h4 className=" m-0 font-bold text-zinc-400">{formattedDate}</h4>
+      <div className="flex flex-col gap-2">
+        <a href={`/${slug}`} className="m-0 rounded no-underline">
+          <h1 className="m-0 hover:text-amber-900 dark:hover:text-amber-300">
+            {title}
+          </h1>
+        </a>
+        <p className="m-0">{excerpt}</p>
+        <Metadata className="m-0" readtime={readtime} />
+        <Tags className="m-0" tags={tags} />
+      </div>
     </div>
   )
 }
