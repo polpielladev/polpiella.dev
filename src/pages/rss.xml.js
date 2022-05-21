6e858cd4e302df252b6d2d0d12291451ab726c87
Pol Piella Abadia <hi@polpiella.dev>
2022-05-21 10:41:56 +0100

    Adds new `rss` route for generation

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,9 @@
+import rss from '@astrojs/rss';
+
+export const get = () => rss({
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
+    site: import.meta.env.SITE,
+    items: import.meta.glob('./*.md'),
+    customData: `<language>en-us</language>`,
+});
