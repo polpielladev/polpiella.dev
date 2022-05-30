3cc804b2df1bd82c357279b1f38689bc4c693b55
Pol Piella Abadia <hi@polpiella.dev>
2022-05-30 12:25:13 +0200

    Updates RSS feed to fix issues

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,25 @@
 import rss from '@astrojs/rss'
 
-export const get = () =>
-  rss({
+const postImportResult = import.meta.globEager('./*.md')
+const posts = Object.values(postImportResult)
+
+export const get = async () => {
+  const htmls = await Promise.all(
+    posts.map(async (post) => await post.compiledContent())
+  )
+
+  const rssItems = posts.map((post, index) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.pubDate,
+    description: htmls[index],
+  }))
+
+  return rss({
     title: "Pol Piella's Blog",
-    description: 'A blog about iOS app development, software development, and other things.',
+    description: 'A blog about iOS mobile app development and Swift.',
     site: 'https://polpiella.dev',
-    items: import.meta.glob('./*.md'),
+    items: rssItems,
     customData: `<language>en-us</language>`,
   })
+}
