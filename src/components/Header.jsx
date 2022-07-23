7c5071bc67c30718bd77d943cc1142387e3e7565
Pol Piella Abadia <hi@polpiella.dev>
2022-07-23 09:38:06 +0100

    Adds `gap` property to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Social from './Social'
 export default function Header() {
   return (
     <header className="w-100 mb-16 bg-gray-100 px-4 dark:bg-gray-800">
-      <div className="w-100 mx-auto flex max-w-screen-lg items-center justify-between">
+      <div className="w-100 mx-auto flex max-w-screen-lg items-center justify-between gap-4">
         <a
           className="flex items-center text-xl font-bold transition-transform hover:scale-105 dark:text-white"
           href="/">
