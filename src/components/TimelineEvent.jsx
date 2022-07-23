2d98c598a8ac2677c94281dc305edc21872f5ac5
Pol Piella Abadia <hi@polpiella.dev>
2022-07-23 09:34:36 +0100

    Replaces `class` with `className` in `jsx` file

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -9,29 +9,29 @@ export default function TimelineEvent({
   endDate,
 }) {
   return (
-    <li class="mb-10 ml-5">
+    <li className="mb-10 ml-5">
       <span
-        class="absolute -left-5 flex h-10 w-10 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900"
+        className="absolute -left-5 flex h-10 w-10 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900"
         style={{ backgroundColor: background }}>
         <img className="h-5 w-5" src={image} />
       </span>
-      <h3 class="m-0 mb-2 flex items-center font-semibold text-gray-900 dark:text-white">
+      <h3 className="m-0 mb-2 flex items-center font-semibold text-gray-900 dark:text-white">
         {title}
       </h3>
-      <h4 class="m-0 mb-2 leading-none">{company}</h4>
-      <time class="mb-4 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+      <h4 className="m-0 mb-2 leading-none">{company}</h4>
+      <time className="mb-4 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
         {startDate} - {endDate ?? 'Present'}
       </time>
 
-      <p class="m-0 text-base font-normal text-gray-500 dark:text-gray-400">
+      <p className="m-0 text-base font-normal text-gray-500 dark:text-gray-400">
         {description}
       </p>
       {metadataLink && (
         <a
           href="#"
-          class="inline-flex items-center rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+          className="inline-flex items-center rounded-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
           <svg
-            class="mr-2 h-4 w-4"
+            className="mr-2 h-4 w-4"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg">
