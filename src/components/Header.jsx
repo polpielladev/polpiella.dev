deede6d88a93322118715f753aac16531218d152
Pol Piella Abadia <hi@polpiella.dev>
2022-07-31 19:54:58 +0100

    Adds contact me button for sending emails

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,16 @@ export default function Header() {
           <span className=" ml-1 rounded bg-amber-400 p-1 text-black">DEV</span>
         </a>
         <Social />
-        <a
-          href="/about"
-          className="m-0 inline-flex items-center rounded-lg bg-amber-400 py-2 px-4 text-center text-sm font-bold text-black no-underline hover:bg-amber-300 focus:outline-none focus:ring-4">
-          About Me
-        </a>
+        <div className="prose flex items-center gap-3 no-underline dark:prose-invert">
+          <a className="no-underline" href="/about">
+            About me
+          </a>
+          <a
+            href="mailto:hi@polpiella.dev"
+            className="m-0 inline-flex items-center rounded-lg bg-amber-400 py-2 px-4 text-center text-sm font-bold text-black no-underline hover:bg-amber-300 focus:outline-none focus:ring-4">
+            Contact Me
+          </a>
+        </div>
       </div>
     </header>
   )
