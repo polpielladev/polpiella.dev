feb89eb6f01f63a8da9dbe2845c4af1930aa99d3
Pol Piella Abadia <hi@polpiella.dev>
2022-07-31 20:24:09 +0100

    Addresses issues with layout on small devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import Social from './Social'
 export default function Footer() {
   return (
     <footer className="mt-16 mb-0 flex flex-col bg-gray-100 align-middle dark:bg-gray-800">
-      <Social />
+      <div className="p-5">
+        <Social />
+      </div>
       <p className=" mb-1 text-center text-gray-600 dark:text-gray-300">
         Made with 💛 by Pol Piella Abadia
       </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import Social from './Social'
 
 export default function Header() {
   return (
-    <header className="w-100 mb-16 bg-gray-100 px-4 dark:bg-gray-800">
-      <div className="w-100 mx-auto flex max-w-screen-lg items-center justify-between gap-4">
+    <header className="mb-16 bg-gray-100 px-4 dark:bg-gray-800">
+      <div className="mx-auto flex max-w-screen-lg items-center justify-between gap-4 py-4">
         <a
-          className="flex items-center text-xl font-bold transition-transform hover:scale-105 dark:text-white"
+          className="text-md flex items-center font-bold transition-transform hover:scale-105 dark:text-white sm:text-xl"
           href="/">
           polpiella
           <span className=" ml-1 rounded bg-amber-400 p-1 text-black">DEV</span>
         </a>
-        <Social />
+        <div className="hidden sm:block">
+          <Social />
+        </div>
         <div className="prose flex items-center gap-3 no-underline dark:prose-invert">
           <a className="no-underline" href="/about">
             About me
