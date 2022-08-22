1a786b459befbfbc68038be36ca1151dbb61a5c2
Pol Piella Abadia <hi@polpiella.dev>
2022-08-22 11:00:41 +0100

    Fixes warnings on bad naming for react

--- src/components/BlogPreview.jsx
+++ src/components/BlogPreview.jsx
@@ -24,9 +24,9 @@ export default function BlogPreview({ post }) {
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                clip-rule="evenodd"></path>
+                clipRule="evenodd"></path>
             </svg>
             {readtime} minute read
           </span>
