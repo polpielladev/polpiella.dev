d50a538435c1bde5c2cf1149489a97dc2fc39150
Pol Piella <24246926+pol-piella@users.noreply.github.com>
2022-11-11 22:59:01 +0000

    Creates speaking page

--- src/components/Talk.jsx
+++ src/components/Talk.jsx
@@ -1,23 +1,22 @@
-export default function Talk({ title, location, excerpt, slides = null }) {
+export default function Talk({
+  title,
+  event,
+  date,
+  slides = null,
+  code = null,
+  video = null,
+}) {
   return (
-    <div className="grid justify-items-center rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
-      <div className="max-w-sm">
-        <h2 className="m-0 mb-1 font-bold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h2>
-        <p className="m-0 mb-3 font-bold text-gray-400 dark:text-gray-300">
-          📍 {location}
-        </p>
-        <p className=" m-0 mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">
-          {excerpt}
-        </p>
-        {slides && (
-          <a
-            href={slides}
-            className="m-0 inline-flex items-center rounded-lg bg-amber-400 py-2 px-4 text-center text-sm font-bold text-black no-underline hover:bg-amber-300 focus:outline-none focus:ring-4">
-            Download Slides &darr;
-          </a>
-        )}
+    <div class="flex gap-1">
+      <div class="flex flex-col gap-1">
+        <h3 class="m-0">{title}</h3>
+        <p class="m-0">{`📍 ${event}`}</p>
+        <p class="m-0">{`🗓️ ${date}`}</p>
+        <div class="flex flex-wrap gap-2">
+          {slides && <a>Slides</a>}
+          {code && <a>Code</a>}
+          {video && <a>Watch</a>}
+        </div>
       </div>
     </div>
   )
