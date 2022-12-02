3b2dd15c72532cf04f0e845a7b59dacbc41b81e5
Pol Piella Abadia <hi@polpiella.dev>
2022-12-02 19:57:55 +0000

    Updating html to fix issues

--- api/thumbnail.js
+++ api/thumbnail.js
@@ -23,23 +23,23 @@ export default async (req, res) => {
   const markup = html`<html>
   <body style="margin: 0; padding: 0">
       <div style="display: flex; position: relative; flex-direction: column; width: 100vw; height: 100vh;">
+          <img
+            src="https://polpiella.dev/assets/og-background.png"
+            style="position:absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; object-position: center;"
+          />
           <div style="width: 100%; height: 100%; display: flex; align-items: center; margin-left: 150px;">
               <h1 style="width: 100%; color: #FFFBFB; max-width: 1000px; font-size: 85px; line-height: 102.87px; font-weight: 500;">
                   ${title}
               </h1>
           </div>
-          <img
-            src="https://polpiella.dev/assets/og-background.png"
-            style="position:absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; object-position: center; z-index: -1;"
-          />
           <div style="display: flex; position: absolute; bottom: 20px; right: 40px; align-items: center; gap: 20px;">
               <img
                   src="https://polpiella.dev/assets/profile.jpg"
                   style="width: 100px; height: 100px; object-fit: cover; border-radius: 100px;"
               />
-              <div style="color: #FFFBFB; display: flex; flex-direction: column; gap: 5px;">
-                  <p style="font-size: 20px;">Pol Piella Abadia</p>
-                  <p style="color: #e5e7eb;">@polpielladev</p>
+              <div style="color: #FFFBFB; display: flex; flex-direction: column; gap: 10px;">
+                  <p style="font-size: 20px; margin: 0;">Pol Piella Abadia</p>
+                  <p style="color: #e5e7eb; margin: 0;">@polpielladev</p>
               </div>
           </div>
       </div>
