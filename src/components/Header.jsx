49638bb4b4337af85a2d8bcb467618f7e67e0977
Pol Piella Abadia <hi@polpiella.dev>
2022-12-02 16:43:53 +0000

    Introduces the concept of `activeNav`

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import Social from '@components/Social'
 
-export default function Header() {
+export default function Header({ activeNav }) {
   return (
     <header className="mb-8 mt-4">
       <div className="mx-auto flex max-w-screen-lg items-center justify-between gap-4 py-4">
-        <div className="prose flex items-center gap-3 no-underline dark:prose-invert">
-          <a href="/">Home</a>
-          <a href="/newsletter">Newsletter</a>
+        <div className="prose flex items-center gap-4 no-underline dark:prose-invert">
+          <a
+            className={`no-underline ${
+              activeNav == 'index' ? 'text-gray-100' : 'text-gray-400'
+            }`}
+            href="/">
+            Home
+          </a>
+          <a
+            className={`no-underline ${
+              activeNav == 'newsletter' ? 'text-gray-100' : 'text-gray-400'
+            }`}
+            href="/newsletter">
+            Newsletter
+          </a>
         </div>
       </div>
     </header>
