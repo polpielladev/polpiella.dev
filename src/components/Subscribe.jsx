df02ec0dd44bed0380a2956d6a05f02a3d8cce9e
Pol Piella Abadia <hi@polpiella.dev>
2022-12-03 11:44:29 +0000

    Fixes `class` warning

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,11 +2,11 @@ import SubscribeInput from '@components/SubscribeInput'
 
 export default function Subscribe() {
   return (
-    <div class="rounded-md border-2 py-4 px-8">
-      <p class="m-0 text-lg font-bold text-gray-900 dark:text-gray-100 md:text-xl">
+    <div className="rounded-md border-2 py-4 px-8">
+      <p className="m-0 text-lg font-bold text-gray-900 dark:text-gray-100 md:text-xl">
         Subscribe to the iOS CI Newsletter
       </p>
-      <p class="m-0 mb-4 text-gray-800 dark:text-gray-200">
+      <p className="m-0 mb-4 text-gray-800 dark:text-gray-200">
         A fortnightly independent newsletter gathering all updates and news
         about Continuous Integration and Continuous Delivery from the iOS
         community.
