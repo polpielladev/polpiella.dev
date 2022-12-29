06bc5f32e4198140edca7f97468d8606a4f21460
Pol Piella Abadia <hi@polpiella.dev>
2022-12-29 22:08:37 +0100

    Adds support for switching between dark and light mode

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,26 @@
+
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  const handleClick = () => {
+    console.log(theme)
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <button onClick={handleClick} className="w-10 aspect-square">
+        <img src={theme === "light" ? "assets/moon.svg" : "assets/sun.svg"} />
+    </button>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
   content: ['./src/**/*.{html,js,astro,jsx,ts,tsx}'],
+  darkMode: "class",
   plugins: [require('@tailwindcss/typography')],
 }
