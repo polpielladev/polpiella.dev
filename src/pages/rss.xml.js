670ab9f5e0ba6544734c1a74611711f1cbf42797
pol-piella <hi@polpiella.dev>
2023-01-23 18:25:03 +0000

    Makes rss work again

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,29 +1,27 @@
 import rss from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+import { getCollection } from 'astro:content'
 
-const postImportResult = import.meta.globEager('./*.mdx')
-const posts = Object.values(postImportResult).sort(
-  (a, b) =>
-    new Date(b.frontmatter.pubDate).valueOf() -
-    new Date(a.frontmatter.pubDate).valueOf()
-)
+const parser = new MarkdownIt()
 
 export const get = async () => {
-  const htmls = await Promise.all(
-    posts.map(async (post) => await post.compiledContent())
-  )
-
-  const rssItems = posts.map((post, index) => ({
-    link: post.url,
-    title: post.frontmatter.title,
-    pubDate: post.frontmatter.pubDate,
-    description: htmls[index],
-  }))
+  const allBlogPosts = await getCollection('blog')
+  const sortedPosts = allBlogPosts
+    .sort(
+      (a, b) =>
+        new Date(b.frontmatter.pubDate).valueOf() -
+        new Date(a.frontmatter.pubDate).valueOf()
+    )
 
   return rss({
-    title: "Pol Piella's Blog",
+    title: 'Pol Piella Blog',
     description: 'A blog about iOS mobile app development and Swift.',
     site: 'https://polpiella.dev',
-    items: rssItems,
-    customData: `<language>en-us</language>`,
+    items: sortedPosts.map((post) => ({
+      link: `/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
+    })),
   })
 }
