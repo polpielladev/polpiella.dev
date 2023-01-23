3dbd99dbddcf1dff670495ad0153dd5872bf8b0f
pol-piella <hi@polpiella.dev>
2023-01-23 10:41:05 +0000

    Adds `astro-layouts` plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 import { remarkReadingTime } from './src/plugins/remarkReadingTime'
 import { remarkSlugify } from './src/plugins/remarkSlugify'
+import astroLayouts from 'astro-layouts'
+
+const layoutOptions = {
+  "pages/*.md": "/src/layouts/BlogPostLayout.astro",
+  "pages/newsletter/*.md": "/src/layouts/IssueLayout.astro"
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://polpiella.dev',
   markdown: {
-    remarkPlugins: ['remark-code-titles', remarkReadingTime, remarkSlugify],
+    remarkPlugins: ['remark-code-titles', remarkReadingTime, remarkSlugify, [astroLayouts, layoutOptions]],
     rehypePlugins: [
       'rehype-slug',
       [
