0c29063ad556895520f0f7bf06029d74f4913983
Pol Piella Abadia <hi@polpiella.dev>
2023-01-23 19:49:03 +0000

    Migrates newsletter's RSS to use astro colections

--- src/pages/newsletter/rss.xml.js
+++ src/pages/newsletter/rss.xml.js
@@ -1,30 +1,28 @@
 import rss from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+import { getCollection } from 'astro:content'
 
-const issuesImportResult = import.meta.globEager('./*.mdx')
-const issues = Object.values(issuesImportResult).sort(
-  (a, b) =>
-    new Date(b.frontmatter.date).valueOf() -
-    new Date(a.frontmatter.date).valueOf()
-)
+const parser = new MarkdownIt()
 
 export const get = async () => {
-  const htmls = await Promise.all(
-    issues.map(async (issue) => await issue.compiledContent())
+  const allNewsletterIssues = await getCollection('newsletter')
+  const sortedIssues = allNewsletterIssues.sort(
+    (a, b) =>
+      new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf()
   )
-
-  const rssItems = issues.map((issue, index) => ({
-    link: issue.url,
-    title: `iOS CI Newsletter - Issue #${issue.frontmatter.number}`,
-    pubDate: issue.frontmatter.date,
-    description: htmls[index],
-  }))
 
   return rss({
     title: 'iOS CI Newsletter',
     description:
-      'A fortnightly independent newsletter gathering all updates and news about Continuous Integration and Continuous Delivery from the iOS community.',
+      'A fortnightly newsletter gathering all updates and news about Continuous Integration and Continuous Delivery from the iOS community.',
     site: 'https://polpiella.dev/newsletter',
-    items: rssItems,
+    items: sortedIssues.map((issue) => ({
+      title: `iOS CI Newsletter - Issue #${issue.data.number}`,
+      link: `/newsletter/${issue.data.number}`,
+      content: sanitizeHtml(parser.render(issue.body)),
+      pubDate: issue.data.date,
+    })),
     customData: `<language>en-us</language>`,
   })
 }
