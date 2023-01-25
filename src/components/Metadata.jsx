eb87faf113a85301d8afa63f1dc130ba423248b1
pol-piella <hi@polpiella.dev>
2023-01-25 14:48:57 +0000

    Fixes formatting

--- src/components/Metadata.jsx
+++ src/components/Metadata.jsx
@@ -1,15 +1,18 @@
 export default function Metadata({ filePath }) {
   return (
     <div className="flex flex-wrap gap-3 align-middle text-sm">
-        <a
-          id="gh-link"
-          className="relative flex content-center items-center gap-1 no-underline"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://github.com/pol-piella/polpiella.dev/edit/main/${filePath}`}>
-          <img className="m-0 w-5 aspect-square dark:invert" src="/assets/github.svg" />
-          <b>Found a mistake? Edit on Github!</b>
-        </a>
+      <a
+        id="gh-link"
+        className="relative flex content-center items-center gap-1 no-underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://github.com/pol-piella/polpiella.dev/edit/main/${filePath}`}>
+        <img
+          className="m-0 aspect-square w-5 dark:invert"
+          src="/assets/github.svg"
+        />
+        <b>Found a mistake? Edit on Github!</b>
+      </a>
     </div>
   )
 }
