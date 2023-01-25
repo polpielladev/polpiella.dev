b19e829f5ded34ddfccd65f2e7f944a18dbe2061
pol-piella <hi@polpiella.dev>
2023-01-25 14:47:05 +0000

    Re-adds `Metadata` component in blog posts

--- src/components/Metadata.jsx
+++ src/components/Metadata.jsx
@@ -1,17 +1,15 @@
-export default function Metadata({ slug }) {
+export default function Metadata({ filePath }) {
   return (
-    <div className="flex flex-wrap gap-3 align-middle text-xs">
-      {slug && (
+    <div className="flex flex-wrap gap-3 align-middle text-sm">
         <a
           id="gh-link"
           className="relative flex content-center items-center gap-1 no-underline"
           target="_blank"
           rel="noopener noreferrer"
-          href={`https://github.com/pol-piella/polpiella.dev/edit/main/src/pages/${slug}.md`}>
-          <img className="m-0 w-5 dark:invert" src="/assets/github.svg" />
+          href={`https://github.com/pol-piella/polpiella.dev/edit/main/${filePath}`}>
+          <img className="m-0 w-5 aspect-square dark:invert" src="/assets/github.svg" />
           <b>Found a mistake? Edit on Github!</b>
         </a>
-      )}
     </div>
   )
 }
