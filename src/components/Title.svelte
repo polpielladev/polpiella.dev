e4efa7435b71e8e3ec6f73d9838f104b9d93e21a
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 09:43:53 +0000

    Rewrites Title component

---
+++ src/components/Title.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let title
+  export let date
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    dateStyle: 'full',
+  })
+</script>
+
+<header class=" mb-4 flex flex-col items-center justify-center gap-1">
+  <h2 class="m-0 text-center font-sans text-2xl font-semibold sm:text-3xl">
+    {title}
+  </h2>
+  <time
+    datetime={date}
+    class="mb-3 block text-center text-sm text-zinc-400 sm:text-base">
+    {formattedDate}
+  </time>
+</header>
