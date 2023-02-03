3cbaa41d77a530773c9c113920a012e806e2d2c8
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 11:52:46 +0000

    Updates succes/failure newsletter components

--- src/components/NewsletterPane.svelte
+++ src/components/NewsletterPane.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  export let title
-  export let children
+  export let title: string
+  export let success: boolean = false
 </script>
 
 <div
@@ -10,5 +10,35 @@
     src="/assets/ci-newsletter.svg"
     class="mx-auto my-0 w-72"
     alt="iOS CI Newsletter logo" />
-  {children}
+  {#if success}
+    <h3 class="mt-0">ℹ️ Before you go:</h3>
+    <p>
+      Here are some extra steps you can do in your email client to ensure you
+      don't miss a single issue from the iOS CI Newsletter (
+      <strong>hi@polpiella.dev</strong>):
+    </p>
+    <ul class="mt-0">
+      <li>
+        <strong>Gmail:</strong> Add <strong>hi@polpiella.dev</strong> to your primary
+        inbox.
+      </li>
+      <li>
+        <strong>Apple Mail:</strong> Add <strong>hi@polpiella.dev</strong> as a VIP
+        contact.
+      </li>
+      <li>
+        <strong>Outlook:</strong> Add <strong>hi@polpiella.dev</strong> as a favourite.
+      </li>
+    </ul>
+  {/if}
+
+  {#if !success}
+    <p class="text-center">
+      <strong>You've successfully unsubscribed</strong>. Remember that you can
+      resubscribe at any point by going to the
+      <a href="/newsletter">newsletter page</a>.
+    </p>
+  {/if}
+
+  <slot />
 </div>
