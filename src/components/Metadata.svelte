ea5f66d54b616b2e6f036022d8fad981e7655c70
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 09:05:40 +0000

    Rewrites `Metadata` component

---
+++ src/components/Metadata.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let filePath
+</script>
+
+<div class="not-prose flex flex-wrap align-middle text-sm">
+  <a
+    id="gh-link"
+    class="relative flex content-center items-center gap-1 no-underline hover:underline"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={`https://github.com/pol-piella/polpiella.dev/edit/main/${filePath}`}>
+    <img
+      class="m-0 aspect-square w-5 dark:invert"
+      src="/assets/github.svg"
+      alt="Github Logo" />
+    <b>Found a mistake? Edit on Github!</b>
+  </a>
+</div>
