dab22ac166e970aed54cdff43caeacde3de6a002
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 09:08:24 +0000

    Updates Newsletter Pane

--- src/components/NewsletterPane.jsx
+++
@@ -1,9 +0,0 @@
-export default function NewsletterPane({ title, children }) {
-  return (
-    <div className="prose m-auto flex w-full flex-col px-4 dark:prose-invert sm:max-w-xl">
-      <h1 className="m-0 mx-auto">{title}</h1>
-      <img src="/assets/ci-newsletter.svg" className="mx-auto my-0 w-72" />
-      {children}
-    </div>
-  )
-}
---
+++ src/components/NewsletterPane.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let title
+  export let children
+</script>
+
+<div
+  class="prose m-auto flex w-full flex-col px-4 dark:prose-invert sm:max-w-xl">
+  <h1 class="m-0 mx-auto">{title}</h1>
+  <img
+    src="/assets/ci-newsletter.svg"
+    class="mx-auto my-0 w-72"
+    alt="iOS CI Newsletter logo" />
+  {children}
+</div>
