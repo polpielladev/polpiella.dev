612d29a8cda977e7623d9e27a8ba858fbad83198
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 09:01:47 +0000

    Rewrites footer using svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Social from '@components/Social.svelte'
+</script>
+
+<footer class="mt-16 mb-8 flex flex-col align-middle">
+  <hr class="w-full" />
+  <div class="flex w-full justify-center p-5 align-middle">
+    <Social />
+  </div>
+  <p class=" mb-5 text-center text-gray-600 dark:text-gray-300">
+    Made with 💛 by Pol Piella Abadia
+  </p>
+</footer>
