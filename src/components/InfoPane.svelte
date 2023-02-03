47c760d6756b486d87b193a0fa5f7d3d57c7ceaf
Pol Piella Abadia <hi@polpiella.dev>
2023-02-03 09:03:04 +0000

    Updates InfoPane and Social components

---
+++ src/components/InfoPane.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Social from '@components/Social.svelte'
+
+  export let title
+  export let subtitle = null
+  export let image
+  export let alt
+  export let description
+  export let circleImage = false
+</script>
+
+<div class="flex w-full flex-col gap-8 text-left md:flex-row">
+  <img
+    class={`m-0 h-28 w-28 ${
+      circleImage ? 'rounded-full' : ''
+    } block justify-start object-cover md:hidden`}
+    src={image}
+    {alt} />
+  <div class="prose grid content-center dark:prose-invert">
+    <div class="mb-4">
+      <h1 class={`m-0 ${subtitle == null ? 'mb-4' : ''}`}>{title}</h1>
+      {#if subtitle}
+        <h2
+          class="m-0 mb-2 text-lg font-normal text-gray-600 dark:text-gray-200">
+          {subtitle}
+        </h2>
+      {/if}
+      <Social size={24} />
+    </div>
+    <p class="m-0 text-gray-500 dark:text-gray-400">{description}</p>
+  </div>
+  <img
+    class={`m-0 aspect-square h-40 w-40 ${
+      circleImage ? 'rounded-full' : ''
+    } hidden object-cover md:block`}
+    src={image}
+    {alt} />
+</div>
---
+++ src/components/Social.svelte
@@ -0,0 +1,48 @@
+<script>
+  const SOCIAL_MEDIA = [
+    {
+      asset: '/assets/github.svg',
+      url: 'https://github.com/pol-piella',
+      alt: 'Github Logo',
+      rel: '',
+    },
+    {
+      asset: '/assets/linkedin.svg',
+      url: 'https://www.linkedin.com/in/pol-piella-81b846115/',
+      alt: 'Linkedin Logo',
+      rel: '',
+    },
+    {
+      asset: '/assets/twitter.svg',
+      url: 'https://twitter.com/polpielladev',
+      alt: 'Twitter Logo',
+      rel: '',
+    },
+    {
+      asset: '/assets/mastodon.svg',
+      url: 'https://iosdev.space/@polpielladev',
+      alt: 'Mastodon Logo',
+      rel: 'me',
+    },
+  ]
+
+  export let size = 25
+</script>
+
+<div class="flex gap-8">
+  {#each SOCIAL_MEDIA as social}
+    <a
+      class="transition-transform hover:scale-110 dark:invert"
+      key={social.url}
+      target="_blank"
+      rel={`noopener noreferrer ${social.rel}`}
+      href={social.url}>
+      <img
+        width={`${size}px`}
+        height={`${size}px`}
+        class="m-0"
+        src={social.asset}
+        alt={social.alt} />
+    </a>
+  {/each}
+</div>
