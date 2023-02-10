7f409b91476c390d8538313af096404111bec9a5
Pol Piella Abadia <hi@polpiella.dev>
2023-02-10 16:02:20 +0000

    Adds image to talk component

--- src/components/Talk.svelte
+++ src/components/Talk.svelte
@@ -5,9 +5,14 @@
   export let slides: string | null = null
   export let code: string | null = null
   export let video: string | null = null
+  export let icon: string | null = null
 </script>
 
-<div class="flex gap-1">
+<div class="flex items-start gap-3">
+  <img
+    class="not-prose m-0 mt-2 aspect-square w-12 rounded-sm object-contain"
+    src={icon ?? '/assets/conferences/placeholder.webp'}
+    alt="Conference icon" />
   <div class="flex flex-col gap-1">
     <h3 class="m-0">{title}</h3>
     <p class="m-0">{`📍 ${event}`}</p>
