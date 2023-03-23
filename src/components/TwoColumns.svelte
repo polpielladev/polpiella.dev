a27f840a1a90eb9eac2b51e93d9734dfe8553ae0
Pol Piella Abadia <hi@polpiella.dev>
2023-03-23 17:14:34 +0000

    Makes a `TwoColumn` component

---
+++ src/components/TwoColumns.svelte
@@ -0,0 +1,9 @@
+<div class="my-0 flex gap-12">
+  <div class="flex flex-shrink flex-grow basis-[0%] flex-col gap-8">
+    <slot name="main" />
+  </div>
+  <aside
+    class="prose hidden flex-shrink-0 flex-grow-0 basis-[320px] flex-col gap-8 dark:prose-invert md:flex">
+    <slot name="aside" />
+  </aside>
+</div>
