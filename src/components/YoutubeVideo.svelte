09ed1b6b78637ad6cea25a2bf700e72ee3c8c029
Pol Piella Abadia <hi@polpiella.dev>
2023-07-23 11:03:02 +0100

    Allows playback directly from the website

---
+++ src/components/YoutubeVideo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let id: string
+</script>
+
+<iframe
+  class="aspect-video w-full"
+  src={`https://www.youtube.com/embed/${id}`}
+  title="Youtube Video Playback components"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen />
