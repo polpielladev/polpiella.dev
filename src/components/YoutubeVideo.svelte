f6a45d1bbc1f292229d13aac1a4ca90b1cdffd49
Pol Piella Abadia <hi@polpiella.dev>
2023-07-23 19:32:44 +0100

    Uses `LiteYoutube` and only loads when visible

--- src/components/YoutubeVideo.svelte
+++ src/components/YoutubeVideo.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
+  import LiteYouTube from 'svelte-lite-youtube-embed'
   export let id: string
 </script>
 
-<iframe
-  class="aspect-video w-full"
-  src={`https://www.youtube.com/embed/${id}`}
-  title="Youtube Video Playback components"
-  frameborder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowfullscreen />
+<LiteYouTube videoId={id} />
