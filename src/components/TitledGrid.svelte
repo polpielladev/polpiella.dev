66b4230cae9af68f230bf51ab4a353f751848431
pol-piella <hi@polpiella.dev>
2023-07-24 09:18:45 +0100

    Componetize index screen

---
+++ src/components/TitledGrid.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  interface Disclose {
+    title: string
+    link: string
+  }
+
+  export let title: string
+  export let disclose: Disclose | undefined = undefined
+</script>
+
+<section class="flex flex-col gap-6">
+  <div>
+    <div class="flex justify-between">
+      <h2 class="flex pb-1 font-title text-2xl dark:text-gray-50">
+        {title}
+      </h2>
+      {#if disclose}
+        <a
+          href={disclose.link}
+          class="flex gap-0.5 stroke-current text-violet-500 hover:text-violet-600 hover:underline dark:text-violet-200 dark:hover:text-violet-100">
+          {disclose.title}
+          <svg
+            class="w-4"
+            width="24px"
+            height="24px"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            color="current"
+            ><path
+              d="M9 6l6 6-6 6"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round" />
+          </svg>
+        </a>
+      {/if}
+    </div>
+
+    <hr />
+  </div>
+
+  <div class="grid gap-6 pt-4 sm:grid-cols-2 lg:grid-cols-3">
+    <slot />
+  </div>
+</section>
