ab6a8777988303f37b8e7f70aca90d602989f9dd
Pol Piella Abadia <hi@polpiella.dev>
2023-07-27 08:50:26 +0100

    Fixes more accessibility issues

--- src/components/BlogPreview.svelte
+++ src/components/BlogPreview.svelte
@@ -16,10 +16,10 @@
       class="absolute -top-7 mr-2 rounded-md bg-purple-100 px-2 py-0.5 text-xs font-semibold text-violet-800 dark:bg-violet-900 dark:text-purple-300"
       >New!</span>
   {/if}
-  <h4
+  <header
     class="text-theme-primary m-0 font-title text-xl hover:text-violet-700 dark:text-gray-100 dark:hover:text-violet-400">
     <a href={`/${slug}`} {title}>{title}</a>
-  </h4>
+  </header>
   <p class="hyphens-auto text-lg leading-6 line-clamp-3 dark:text-gray-400">
     {excerpt}
   </p>
