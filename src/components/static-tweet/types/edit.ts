5c0cea78543cf5f0abced76755bc12029f688cbf
Pol Piella Abadia <hi@polpiella.dev>
2024-02-21 23:55:10 +0100

    Fixes CI/CD

--- src/components/static-tweet/types/edit.ts
+++
@@ -1,6 +0,0 @@
-export interface TweetEditControl {
-  edit_tweet_ids: string[]
-  editable_until_msecs: string
-  is_edit_eligible: boolean
-  edits_remaining: string
-}
--- src/components/static-tweet/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface TweetUser {
-  id_str: string
-  name: string
-  profile_image_url_https: string
-  screen_name: string
-  verified: boolean
-  verified_type: string
-  is_blue_verified: boolean
-}
