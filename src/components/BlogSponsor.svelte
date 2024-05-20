45bdc478cfcb79f2710ef4dd487f73e5c6b1ff03
Pol Piella Abadia <hi@polpiella.dev>
2024-05-20 18:30:26 +0200

    New sponsor

--- src/components/BlogSponsor.svelte
+++ src/components/BlogSponsor.svelte
@@ -7,11 +7,11 @@
     variant === 'default'
       ? 'flex-col gap-2 content-center justify-center px-2 py-4 text-center'
       : 'grid-cols-[5rem_1fr] p-2 gap-2 align-middle'
-  } rounded-lg border-2 border-[#f2545b] bg-[#f2545b]/10 shadow-lg `}>
+  } rounded-lg border-2 border-[#5BC69B] bg-[#5BC69B]/10 shadow-lg `}>
   <p
     class={`absolute right-2 ${
       variant === 'default' ? 'top-2' : 'top-[-11px]'
-    } mx-auto rounded-md bg-[#f2545b] px-2 py-0.5 font-title text-xs text-white shadow`}>
+    } mx-auto rounded-md bg-[#5BC69B] px-2 py-0.5 font-title text-xs text-white shadow`}>
     Sponsored
   </p>
 
@@ -21,13 +21,13 @@
         width="96"
         height="96"
         class="mx-auto w-24 aspect-square object-contain"
-        src="/assets/sponsors/rc.png"
-        alt="RevenueCat logo" />
+        src="/assets/sponsors/zenitizer.png"
+        alt="Zenitizer logo" />
   {:else}
     <img
       class={`'w-20' relative mx-auto aspect-square object-contain px-2`}
-      src="/assets/sponsors/rc.png"
-      alt="RevenueCat logo" />
+      src="/assets/sponsors/zenitizer.png"
+      alt="Zenitizer logo" />
   {/if}
 
   <div
@@ -35,17 +35,17 @@
       variant == 'small' ? 'my-auto w-full' : 'flex flex-col gap-0'
     } max-w-lg`}>
     <a
-      href="https://www.revenuecat.com/docs/tools/paywalls?utm_medium=sponsored&utm_source=polpiella.dev&utm_campaign=general_sponsorship&utm_content=revenuecat-paywalls-banner"
+      href="https://apps.apple.com/us/app/meditation-timer-zenitizer/id6444767911?utm_medium=sponsored&utm_source=polpiella.dev&utm_campaign=general_sponsorship&utm_content=zenitizer-banner"
       class={`font-title ${
         variant == 'default' ? 'text-lg' : 'text-md'
       } no-underline hover:underline dark:text-white`}>
-      RevenueCat Paywalls
+      Zenitizer - Meditation Timer
     </a>
     <p
       class={`m-0 ${
         variant == 'default' ? 'text-sm' : 'text-xs'
       } text-sm leading-relaxed dark:text-gray-300`}>
-      Add paywalls to your iOS app's in one line of code! With RevenueCat Paywalls you can remotely configure and edit your entire paywall view without waiting on App Review.
+      Enjoy clutter-free meditation with Zenitizer's clean UI and soothing sounds. For Apple geeks: Shortcuts, Apple Health, widgets and fully-featured watchOS and visionOS apps! Get 25% OFF with code: POL24
     </p>
   </div>
 </div>
