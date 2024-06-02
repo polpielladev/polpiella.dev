9c2ffef9239eafa30d967b49e61f7a7da1b9e1ad
Pol Piella Abadia <hi@polpiella.dev>
2024-06-02 22:32:19 +0200

    Codemagic is the new sponsor!

--- src/components/BlogSponsor.svelte
+++ src/components/BlogSponsor.svelte
@@ -5,47 +5,50 @@
 <div
   class={`not-prose relative grid h-full ${
     variant === 'default'
-      ? 'flex-col gap-2 content-center justify-center px-2 py-4 text-center'
-      : 'grid-cols-[5rem_1fr] p-2 gap-2 align-middle'
-  } rounded-lg border-2 border-[#5BC69B] bg-[#5BC69B]/10 shadow-lg `}>
+      ? 'flex-col content-center justify-center px-2 py-4 text-center'
+      : 'grid-cols-[5rem_1fr] p-2 align-middle'
+  } rounded-lg border-2 border-[#0051FF] bg-[#0051FF]/10 shadow-lg `}>
   <p
     class={`absolute right-2 ${
       variant === 'default' ? 'top-2' : 'top-[-11px]'
-    } mx-auto rounded-md bg-[#5BC69B] px-2 py-0.5 font-title text-xs text-white shadow`}>
+    } mx-auto rounded-md bg-[#0051FF] px-2 py-0.5 font-title text-xs text-white shadow`}>
     Sponsored
   </p>
 
   {#if variant === 'default'}
-  
+    <picture>
+      <source
+        srcset="/assets/sponsors/codemagic-wordmark-white.svg"
+        media="(prefers-color-scheme: dark)" />
       <img
-        width="96"
-        height="96"
-        class="mx-auto w-24 aspect-square object-contain"
-        src="/assets/sponsors/zenitizer.png"
-        alt="Zenitizer logo" />
+        class=" mx-auto w-64 object-contain pb-4 pt-7"
+        src="/assets/sponsors/codemagic-wordmark-blue.svg"
+        alt="Codemagic logo" />
+    </picture>
   {:else}
     <img
       class={`'w-20' relative mx-auto aspect-square object-contain px-2`}
-      src="/assets/sponsors/zenitizer.png"
-      alt="Zenitizer logo" />
+      src="/assets/sponsors/codemagic-star.svg"
+      alt="Codemagic logo" />
   {/if}
 
   <div
     class={`${
-      variant == 'small' ? 'my-auto w-full' : 'flex flex-col gap-0'
-    } max-w-lg`}>
+      variant == 'small' ? 'my-auto' : 'flex flex-col gap-1'
+    } max-w-xs`}>
     <a
-      href="https://apps.apple.com/us/app/meditation-timer-zenitizer/id6444767911?utm_medium=sponsored&utm_source=polpiella.dev&utm_campaign=general_sponsorship&utm_content=zenitizer-banner"
+      href="https://codemagic.io/start/?utm_source=polpielladev&utm_medium=advertisement&utm_campaign=codemagic"
       class={`font-title ${
         variant == 'default' ? 'text-lg' : 'text-md'
       } no-underline hover:underline dark:text-white`}>
-      Zenitizer - Meditation Timer
+      Codemagic CI/CD for mobile teams
     </a>
     <p
       class={`m-0 ${
         variant == 'default' ? 'text-sm' : 'text-xs'
       } text-sm leading-relaxed dark:text-gray-300`}>
-      Enjoy clutter-free meditation with Zenitizer's clean UI and soothing sounds. For Apple geeks: Shortcuts, Apple Health, widgets and fully-featured watchOS and visionOS apps! Get 25% OFF with code: POL24
+      What do you get when you put love for iOS and DevOps together? Answer:
+      Codemagic CI/CD
     </p>
   </div>
 </div>
