065b492f4d86300ce7541bdcd0884b1cf9d5d645
Pol Piella Abadia <hi@polpiella.dev>
2024-07-25 10:22:46 +0200

    Updates all references to use new CDN service

--- src/components/Video.svelte
+++ src/components/Video.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  export let src: string
+  export let name: string
   export let controls = true
+
+  const src = `https://d2thv89fb05cma.cloudfront.net/polpiella.dev/videos/${name}`
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
